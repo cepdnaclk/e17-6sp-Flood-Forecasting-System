e6020b0abfe83daed90ef641b3cb8c0c60d56f69
Esara Sithumal <wmesithumal2@gmail.com>
2022-03-06 18:47:05 +0530

    AB#44 install react-arcgis

--- WebApp/Frontend/flood-forecasting-system/src/App.js
+++ WebApp/Frontend/flood-forecasting-system/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Map } from "@esri/react-arcgis";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Map
+        mapProperties={{ basemap: "satellite" }}
+        viewProperties={{
+          center: [-122.4443, 47.2529],
+          zoom: 6,
+        }}
+      />
     </div>
   );
 }
