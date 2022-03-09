0bf8297ccce532557a149f754d90177ae29a7c65
lakshitha srimal <e17338@eng.pdn.ac.lk>
2022-03-09 13:26:54 +0530

    modifications- homepage, report page, footer

--- WebApp/Frontend/flood-forecasting-system/src/assets/pages/report/report.js
+++ WebApp/Frontend/flood-forecasting-system/src/assets/pages/report/report.js
@@ -1,5 +1,59 @@
+import { Dropdown, DropdownButton } from 'react-bootstrap';
+import React, { useState } from 'react';
+import DatePicker from 'react-date-picker';
+
+import './styles.css';
+
+import { FaFilter } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
+
 export default function Report(){
+
+	//today
+	var today = new Date();
+	var dd = String(today.getDate()).padStart(2, '0');
+	var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+	var yyyy = today.getFullYear();
+
+	const [from, onChange1] = useState(new Date( mm + '/' + dd + '/' + yyyy));
+	const [to, onChange2] = useState(new Date( mm + '/' + dd + '/' + yyyy));
+
 	return(
-		<h1>Report</h1>
+		<>
+			<div className="container report">
+				<div className="row filter">
+					{<FaFilter style={{color: '#766262;', fontSize: '35px', paddingTop: '3px'}} />}
+
+					<DropdownButton id="dropdown-basic-button" title="Select">
+  						<Dropdown.Item className="dropdown-item" >In Flow</Dropdown.Item>
+  						<Dropdown.Item className="dropdown-item" >Out Flow</Dropdown.Item>
+  						<Dropdown.Item className="dropdown-item" >Rain</Dropdown.Item>
+					</DropdownButton>
+
+					<DatePicker onChange={onChange1} value={from} clearIcon={null} className="date"/>
+					<DatePicker onChange={onChange2} value={to} clearIcon={null} className="date"/>
+
+					<button id="search">
+						{<FaSearch style={{color: '#766262;', fontSize: '30px'}} />}
+					</button>
+				</div>
+				<div className="body"></div>
+			</div>
+			{<Links/>}
+		</>
 	);
 }
+
+function Links(){
+	return(
+ 		<div className="external">
+ 			<h5>External Links</h5>
+ 			<br/>
+ 			<div className="row">
+ 				<div className="col-4"><a href="">Link1</a></div>
+ 				<div className="col-4"><a href="">Link2</a></div>
+ 				<div className="col-4"><a href="">Link3</a></div>
+ 			</div>
+ 		</div>
+ 	);
+ }
