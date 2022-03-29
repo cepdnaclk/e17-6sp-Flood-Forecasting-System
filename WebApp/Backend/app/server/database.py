788db2b58c0acf3c2ccbe11e26df2687f7b75e9f
Esara Sithumal <wmesithumal2@gmail.com>
2022-03-29 21:23:53 +0530

    Implement user registration endpoint for SMS alerts

--- WebApp/Backend/app/server/database.py
+++ WebApp/Backend/app/server/database.py
@@ -3,6 +3,6 @@ import motor.motor_asyncio
 
 MONGO_URL = config("MONGO_URL")
 
-client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URL)
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URL, serverSelectionTimeoutMS=5000)
 
 database = client.FFSDB
--- WebApp/Backend/app/server/routes/userRoute.py
+++ WebApp/Backend/app/server/routes/userRoute.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Body
+from fastapi import APIRouter, Body, Response
 from fastapi.encoders import jsonable_encoder
 from app.server.database import database
 from app.server.models.user import (UserScheama , ResponseModel)
@@ -7,11 +7,28 @@ user_collection = database.get_collection("users")
 
 router = APIRouter()
 
-@router.post("/",tags=["User"])
-async def add_user_data(user: UserScheama = Body(...)):
+#this route finds a user by registered phone number
+@router.get("/",tags=["User"])
+async def retrive_user(phoneNumber:int, res : Response):
+    user = await user_collection.find_one({"phoneNumber":phoneNumber})
+    if(user):
+        return ResponseModel(user,"User found")
+    res.status_code = 404
+    return {"message":"User not found"}
+
+#this route is for register a user in the system
+#need phone number and name
+@router.post("/",tags=["User"], status_code = 201)
+async def add_user_data(res: Response ,user: UserScheama = Body(...) ):
     user = jsonable_encoder(user)
-    new_user = await user_collection.insert_one(user)
-    if new_user:
-        return {"message":"user registration succeed"}
+    curr_user = await user_collection.find_one({"phoneNumber":user["phoneNumber"]})
+    if(curr_user):
+        res.status_code = 409
+        return {"message":"The phone number is already registered"}
+    else:
+        new_user = await user_collection.insert_one(user)
+        if new_user:
+            return {"message":"user registration succeed"}
+    res.stats_code = 400
     return {"message":"user registration failed"}
     
